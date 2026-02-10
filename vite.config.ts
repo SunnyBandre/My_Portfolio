c1a57b73eef1aec05e5b596efdbbf2c85b8df696
SunnyBandre <bandresunny89@gmail.com>
2026-02-10 12:19:40 +0530

    chnage

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,15 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: path.resolve(__dirname, "client"),
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "client/src"),
     },
   },
   build: {
-    outDir: "dist",
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
   },
 });
